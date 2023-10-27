959f5dca6ef3248ca076ae1306793b892d983f03
Adekunle Stephen Omorotimi <okste1234@gmail.com>
2023-10-27 09:45:40 +0100

    changing layout

--- app/components/Contents.jsx
+++ app/components/Contents.jsx
@@ -1,21 +1,15 @@
-import React from "react";
 import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Main from "./Main";
 import Footer from "./Footer";
 
-const Contents = () => {
+const Contents = ({ children }) => {
 	return (
 		<div className="-z[1]">
 			<nav className="top-0 sticky w-full bg-base-100 z-50">
 				<Navbar />
 			</nav>
-			<header>
-				<Hero />
-			</header>
-			<section className="relative">
-				<Main />
-			</section>
+			<div>
+				{children}
+			</div>
 			<footer className="">
 				<Footer />
 			</footer>
--- app/page.js
+++ app/page.js
@@ -2,11 +2,20 @@ import Header from './components/Main'
 import Contents from './components/Contents'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Main from './components/Main'
 
 export default function Home() {
   return (
     <main className='font-poppins relative'>
-      <Contents />
+      <Contents>
+        <header>
+          <Hero />
+        </header>
+        <section className="relative">
+          <Main />
+        </section>
+      </Contents>
     </main>
   )
 }
